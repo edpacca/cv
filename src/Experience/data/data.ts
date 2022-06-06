b662a72b3013c62d9c009a11b831b37048b11f34
Eddie Pace <epace@scottlogic.com>
2022-06-06 16:10:30 +0100

    write some content

--- src/Experience/data/data.ts
+++ src/Experience/data/data.ts
@@ -5,7 +5,12 @@ export const associateDev: Employment = {
     company: "Scott Logic",
     dates: "Feb 2022 - present",
     experiences: [
-        "Full stack development on SSO application - challenging ",
+        "Full stack development of green-fields SSO application - working to meet client requirements using AGILE delivery principles",
+        "Writing and maintaining code for all layers of application, producing documentation for API design",
+        "Adhering to strict version control policies (git), working in a small dev team with tight deadlines",
+        "Presenting and demonstrating work to different groups of clients, modifying level of presentation to suit audience knowledge",
+        "Deploying codebase to different AWS environments - regular use of Docker, occassional use of kubectl, helm and CDK for managing deployments",
+        "Delivered graduate CI/CD training to last intake of graduates, worked hard to learn and pass on knowledge of an unfamiliar area"
     ]
 }
 
@@ -14,10 +19,10 @@ export const graduateDev: Employment = {
     company: "Scott Logic",
     dates: "Aug 2021 - Feb 2022",
     experiences: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", 
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "Recognised as a fast learner - transfered early from graduate project to client project",
+        "Developed full stack app in Javacript (React/Redux), and Java (SpringBoot) as part of training",
+        "Worked as part of large team of graduates, coordinating our efforts to develop an internal application - demonstrated strong communication and leadership skills",
+        "Attended talks and workshops on design pattern implementation"
     ]
 }
 
