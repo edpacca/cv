e1844c11057e8a09e06eca1b3a6c7eac5b5a643c
Eddie Pace <epace@scottlogic.com>
2022-06-06 14:42:56 +0100

    fix pdf export kind of

--- src/App.svelte
+++ src/App.svelte
@@ -14,16 +14,17 @@
 	}
 
 	async function generatePdf() {
-		var doc = new jsPDF('portrait', 'pt', 'a4');
+		var doc = new jsPDF('portrait', 'pt', 'a3');
 		var source = window.document.getElementById("page-body");
 		await doc.html(source);
 		doc.save('test.pdf');
 	}
+
 </script>
 
 <div>
 	<!-- <button on:click={switchPage}>next page</button> -->
-	<button on:click={generatePdf}>PDF</button>
+	<button class="pdf-button" on:click={generatePdf}>PDF</button>
 </div>
 <main>
 	<div class="page-body" id="page-body">
@@ -41,5 +42,12 @@
 <style>
     .header {
 		margin-bottom: 1em;
+	}
+
+	.pdf-button {
+		color: white;
+		background-color: rgb(30, 30, 30);
+		border-radius: 3px;
+		margin: 1em;
 	}
 </style>
