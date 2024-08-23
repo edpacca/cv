9a17dbe6f41c3565c04ec21abc076fd30c832eeb
Eddie Pace <eddie.pace@cydar.co.uk>
2024-08-23 14:51:34 -0700

    add eligible to work indicator

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -2,21 +2,26 @@
   import PhoneVoice from "carbon-icons-svelte/lib/PhoneVoice.svelte";
   import Email from "carbon-icons-svelte/lib/Email.svelte";
   import Home from "carbon-icons-svelte/lib/Home.svelte";
+  import { CheckmarkFilled } from "carbon-icons-svelte";
 
   export let profile;
 </script>
 
 <div class="contact-info">
-    <Email size={24} class="icon"/>
-    <div>{profile.email}</div>
+  <Email size={24} class="icon"/>
+  <div>{profile.email}</div>
 </div >
-  <div class="contact-info">
-    <PhoneVoice size={24} class="icon"/>
-    <div>{profile.phone}</div>
+<div class="contact-info">
+  <PhoneVoice size={24} class="icon"/>
+  <div>{profile.phone}</div>
 </div>
 <div class="contact-info">
-    <Home size={24} class="icon"/>
-    <div>{profile.address}</div>
+  <Home size={24} class="icon"/>
+  <div>{profile.address}</div>
+</div>
+<div class="contact-info">
+  <CheckmarkFilled size={24} class="icon"/>
+  <div>Eligible to work in CA</div>
 </div>
 
 <style>
