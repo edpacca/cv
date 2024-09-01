55b8078a5c11ebf6f2eb0459af4bf220f7bd6ec0
Eddie Pace <eddie@pace.me.uk>
2024-08-31 23:50:51 -0700

    flex skills

--- src/lib/Skills.svelte
+++ src/lib/Skills.svelte
@@ -3,17 +3,20 @@
 </script>
 
 <h2>Skills</h2>
-
 <div class="skills-container">
-    {#each skills as skill, i}
-        <span>
-            {skill}{#if i < skills.length - 1},&nbsp;{/if}
-        </span>
-    {/each}
+    <div>
+        {#each skills as skill, i}
+            <span>
+                {skill}{#if i < skills.length - 1},&nbsp;{/if}
+            </span>
+        {/each}
+    </div>
 </div>
 
 
 <style>
     .skills-container {
+        display: flex;
+        align-items: center;
     }
 </style>
