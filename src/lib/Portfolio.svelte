dbc921fc7adc65dba61aba8212d1810af02195ad
Eddie Pace <eddie.pace@cydar.co.uk>
2024-08-31 23:12:13 -0700

    create portfolio link

---
+++ src/lib/Portfolio.svelte
@@ -0,0 +1,21 @@
+<script>
+    import IconLink from "./components/IconLink.svelte";
+    export let urls;
+</script>
+
+<div class="portfolio">
+    <IconLink icon={"github"} link={urls.github} text={"Github"}/>
+    <IconLink icon={"linkedin"} link={urls.LinkedIn} text={"LinkedIn"}/>
+    <IconLink icon={"research-gate"} link={urls.researchGate} text={"Research-gate"}/>
+    <IconLink icon={"ep-logo"} link={urls.personal} text={"Personal Website"}/>
+  </div>
+
+<style>
+    .portfolio {
+        display: flex;
+        flex-direction: row;
+        gap: 0.5em;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
