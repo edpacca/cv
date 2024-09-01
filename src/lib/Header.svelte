80e60f0f6cf058433ed0319ca7d62dde9e096cd3
Eddie Pace <eddie.pace@cydar.co.uk>
2024-08-31 23:12:02 -0700

    restructure header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,47 +1,38 @@
 <script>
-    import IconLink from "./components/IconLink.svelte";
+    import { CheckmarkFilled } from "carbon-icons-svelte";
     import Contact from "./Contact.svelte";
 
     export let profile;
-    export let urls;
-
     const profileImgPath = "assets/profile2024.png"
-
 </script>
 
 <header>
   <img src={profileImgPath} alt="Eddie Pace" class="profile-img"/>
-  <div>
     <div class="header-info-container">
       <div>
         <div class="header-title">
           <h1>{profile.name}</h1>
           <h3>{profile.job_title}</h3>
         </div>
-        <div class="portfolio">
-          <IconLink icon={"github"} link={urls.github} text={"Github"}/>
-          <IconLink icon={"linkedin"} link={urls.LinkedIn} text={"LinkedIn"}/>
-          <IconLink icon={"research-gate"} link={urls.researchGate} text={"Research-gate"}/>
-          <IconLink icon={"ep-logo"} link={urls.personal} text={"Personal Website"}/>
-        </div>
       </div>
       <div class="contact-container">
         <Contact profile={profile}/>
       </div>
     </div>
-  </div>
 </header>
 
 <style>
   header {
     display: grid;
-    grid-template-columns: 9em 1fr;
+    grid-template-columns: var(--profile-img-height) 1fr;
     gap: 1em;
+    height: var(--profile-img-height);
+    margin-bottom: 1em;
   }
 
   .profile-img {
-    height: 9em;
     border-radius: 0.5em;
+    height: var(--profile-img-height);
   }
 
   .header-info-container {
@@ -50,8 +41,14 @@
     margin-bottom: 0.5em;
   }
 
+  h3 {
+    font-weight: 300;
+    font-size: 1.4em;
+  }
+
+
   h1 {
-    color: var(--black);
+    text-transform: uppercase;
     text-align: left;
   }
 
@@ -60,14 +57,10 @@
     margin-bottom: 0.5em;
   }
 
-  .portfolio {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5em;
-  }
-
   .contact-container {
-    margin-top: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 0.3em;
   }
 
 </style>
