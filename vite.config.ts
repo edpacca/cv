570b3be6a8de8116bbb243d4ba43b220f4ab4467
Eddie Pace <eddie@pace.me.uk>
2024-09-04 10:40:32 -0700

    fix vite config for root

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cv/",
+  base: "https://edpacca.github.io/cv/",
   plugins: [svelte()],
 })
