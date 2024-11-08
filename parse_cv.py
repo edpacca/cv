298a353d4428b0e17079298929d2d7b559d4c326
Eddie Pace <eddie@pace.me.uk>
2024-11-08 10:51:16 -0800

    add spelling validation to parser

--- parse_cv.py
+++ parse_cv.py
@@ -1,14 +1,73 @@
 #!/usr/bin/env python
 import os
 from dotenv import load_dotenv
-from jsonschema import validate
+from spellchecker import SpellChecker
+import string
 import yaml
 import json
 import datetime
 
+valid_words = [
+    "frontend",
+    "backend",
+    "3d",
+    "architected",
+    "codebase",
+    "containerized",
+    "ci",
+    "gui",
+    "webdriver",
+    "rois",
+    "ct",
+    "2d3d",
+    "github",
+    "slackbot",
+    "pr",
+    "qa",
+    "triaging",
+    "itg",
+    "dicom",
+    "poc",
+    "pkce",
+    "scottish",
+    "sso",
+    "cicd",
+    "phd",
+    "multi",
+    "codebases",
+    "architecting",
+    "datasets"
+]
+
 def DateEncoder(obj):
   if isinstance(obj, (datetime.datetime, datetime.date)):
       return obj.strftime('%Y-%m-%d')
+
+def text_to_wordlist(text):
+    return text.replace("-", " ").translate(str.maketrans('', '', string.punctuation)).split()
+
+def validate_information(data):
+    phone_validator = os.getenv("PHONE")
+    email_validator = os.getenv("EMAIL")
+
+    assert data["profile"]["phone"].replace("-", "") == phone_validator, "phone number incorrect"
+    assert data["profile"]["email"] == email_validator, "email incorrect"
+
+def validate_spelling(data):
+
+    spell = SpellChecker()
+    spell.word_frequency.load_words(valid_words)
+
+    def check_unknown(text, field):
+        unknown = spell.unknown(text_to_wordlist(text))
+        assert len(unknown) == 0, f"\nfound misspelled words in {field}: {unknown}"
+
+    check_unknown(data["summary"], "summary")
+
+    for job in data["experience"]:
+        for note in job["notes"]:
+            check_unknown(note["note"], job["company"])
+
 
 def main():
 
@@ -20,6 +79,9 @@ def main():
 
     with open(f"{input_dir}/{input_file}") as input_file:
         data = yaml.safe_load(input_file)
+
+    validate_information(data)
+    validate_spelling(data)
 
     with open(f"{output_dir}/cv.json", "w") as output_file:
         output_file.write(json.dumps(data, indent=2, default=DateEncoder))
